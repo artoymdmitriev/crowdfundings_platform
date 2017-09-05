7a2dd77b41c6a0c4de1d0f592aaa7dc8aabc7d86
artyom <admitriev5813@gmail.com>
2017-09-06 01:25:43 +0300

    Tests with projects and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905215428) do
+ActiveRecord::Schema.define(version: 20170905221217) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20170905215428) do
     t.boolean  "is_funded",         default: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
