d9dbe17f47fd30dcf781c7e29e80e7dbf510db5b
artyom <admitriev5813@gmail.com>
2017-09-08 01:23:51 +0300

    Add theme engine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :set_locale
+  before_filter :set_theme
 
   private
-  # Before every request, we set the locale, from the specified or detected settings, or from the cookie
   def set_locale
     if language_change_necessary?
       I18n.locale = the_new_locale
@@ -13,30 +13,49 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # A locale change is necessary if no locale cookie is found, or if the locale param has been specified
   def language_change_necessary?
     return cookies['locale'].nil? || params[:locale]
   end
 
-  # The new locale is taken from the current_user language setting, it logged_in, or from the http accept language header if not
-  # In both cases, if a locale param has been passed, it takes precedence. Only available locales are accepted
   def the_new_locale
     new_locale = (params[:locale] || extract_locale_from_accept_language_header)
     ['en', 'ru'].include?(new_locale) ? new_locale : I18n.default_locale.to_s
   end
 
-  # Sets the locale cookie
   def set_locale_cookie(locale)
     cookies['locale'] = locale.to_s
   end
 
-  # Reads the locale cookie and sets the locale from it
   def use_locale_from_cookie
     I18n.locale = cookies['locale']
   end
 
-  # Extracts the locale from the accept language header, if found
   def extract_locale_from_accept_language_header
     locale = request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first rescue I18n.default_locale
+  end
+
+  def set_theme
+    if theme_change_necessary?
+      @theme = the_new_theme
+      set_theme_cookie(@theme)
+    else
+      use_theme_from_cookie
+    end
+  end
+
+  def theme_change_necessary?
+    return cookies['theme'].nil? || params[:theme]
+  end
+
+  def the_new_theme
+    new_theme = (params[:theme] || 'application_light')
+  end
+
+  def set_theme_cookie(theme)
+    cookies['theme'] = theme.to_s
+  end
+
+  def use_theme_from_cookie
+    @theme = cookies['theme']
   end
 end
