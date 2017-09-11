09696d5f37c76835c6d942dd83fa45721904085d
artyom <admitriev5813@gmail.com>
2017-09-11 19:38:58 +0300

    Migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911163559) do
+ActiveRecord::Schema.define(version: 20170910231011) do
 
   create_table "applications", force: :cascade do |t|
     t.string  "name",                         null: false
