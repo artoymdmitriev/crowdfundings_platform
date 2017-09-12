d6e465c1e202d1687f566fbc429e362d2131936c
artyom <admitriev5813@gmail.com>
2017-09-12 20:54:47 +0300

    Add validations to Application and Project

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,3 +1,9 @@
 class Application < ApplicationRecord
   has_one :user
+
+  validates :name, presence: true
+  validates :surname, presence: true
+  validates :pic_link, presence: true
+  validates :birthday, presence: true
+  validates :user_id, presence: true
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,11 @@ class Project < ApplicationRecord
   belongs_to :users, optional: true
   has_many :comments, :as => :commentable
   has_many :payments
+
+  validates :name, presence: true
+  validates :fundings_deadline, presence: true
+  validates :description, presence: true
+  validates :pic_link, presence: true
+  validates :user_id, presence: true
+  validates :goal, presence: true
 end
