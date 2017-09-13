6dcdf2c5d67e7aa2a44e36de29371de7db8e1024
artyom <admitriev5813@gmail.com>
2017-09-13 13:59:15 +0300

    Add displaying of errors (still needs to be fixed)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
 
-  validates :text, presence: true, length: { minimum: 40, message: 'is too short' }
+  validates :text, presence: true, length: { minimum: 4, message: 'is too short' }
   validates :user_id, presence: true
   validates :commentable_id, presence: true
   validates :commentable_type, presence: true
