4207c966682c27c01342dd35fa2a2117b18c3a17
artyom <admitriev5813@gmail.com>
2017-09-19 01:04:23 +0300

    Improve main page

--- app/helpers/main_helper.rb
+++ app/helpers/main_helper.rb
@@ -1,13 +1,17 @@
 module MainHelper
   def last_projects
-    @projects = Project.last(6)
+    Project.last(4)
   end
 
-  def last_users
-    @users = User.last(6)
+  def last_successful_projects
+    Project.where(state: :succeeded).last(4)
+  end
+
+  def biggest_payments
+    Payment.order('amount DESC').first(6)
   end
 
   def last_news_items
-    @news = NewsItem.last(6)
+    NewsItem.order('updated_at DESC').first(6)
   end
 end
