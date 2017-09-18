c1f9df91dc96f2583863d664fcb43ebba4c6e971
artyom <admitriev5813@gmail.com>
2017-09-18 16:31:34 +0300

    Add validations for payments in js script

--- app/models/payment_observer.rb
+++ app/models/payment_observer.rb
@@ -1,7 +1,9 @@
 class PaymentObserver < ActiveRecord::Observer
   observe :payment
 
+  # TODO remove
   def after_create(record)
+    puts "///////////////////////////////"
     project = Project.find(record.project_id)
     project.update(earned: project.earned + record.amount)
   end
