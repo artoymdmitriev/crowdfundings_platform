384285992a7805fed4aea44c9dc95effeed3b22f
artyom <admitriev5813@gmail.com>
2017-09-22 14:17:20 +0300

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,27 @@
 Rails.application.routes.draw do
+  post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
   root to: 'main#index'
 
-
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show]
+  resources :users do
     collection do
       post 'change'
     end
   end
-  resources :projects, has_many: :comments do
+  resources :applications
+  resources :projects do
     resources :comments
     resources :news_items
     resources :payments
+  end
+
+  resources :projects do
+    resources :news_items
     resources :subscriptions, only: [:new, :create, :destroy]
   end
 
-  resources :news_items, has_many: :comments do
+  resources :news_items do
     resources :comments
   end
 
@@ -29,14 +35,18 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :applications
+  resources :payments
+  post 'payments/new'
+
+  resources :comments
+  resources :projects, has_many: :comments
+  resources :news_items
+  get '/settings', to: 'ui_settings#my_settings'
   resources :tags, only: [:index, :show]
 
   post "/subscriptions/create" => "subscriptions#create"
   delete "/subscriptions/destroy" => "subscriptions#destroy"
 
-  post '/rate' => 'rater#create', :as => 'rate'
-  get '/settings', to: 'ui_settings#my_settings'
   get '/my_projects', to: 'projects#my_projects'
   get '/my_news', to: 'news_items#my_news'
 end
